d8587e070f964a8f5c110a8e1428dafb412edccd
Francisco Fernando <fernandocunhananana@gmail.com>
2025-05-31 09:59:55 -0300

    feat: atualização para task 2.

--- teste.py
+++ teste.py
@@ -1,7 +1,33 @@
-while True:
-    print("opções q, 1, 2, 3, 4")
-    menu = input('selecione uma opção ')
-    if menu == 'q' :
-        break 
-     
+açai = 0
+pastel = 0
+xburger = 0
 
+while True:
+    print("opções :")
+    print('(1) pastel')
+    print('(2) açaí')
+    print('(3) xburger')
+    
+    menu = input('selecione uma opção: ')
+    if menu == 'q' :
+        valor_pastel = pastel * 12
+        valor_açai = açai * 11
+        valor_xburguer = xburger * 15
+        valor_final = valor_açai + valor_pastel + valor_xburguer
+        print (f'{pastel} pastéis = R$ {valor_pastel:.2f}') 
+        print (f'{açai} pastéis = R$ {valor_açai:.2f}') 
+        print (f'{xburger} pastéis = R$ {valor_xburguer:.2f}')
+        print(f'Valor final R$ {valor_final:.2f}')
+        break
+
+    elif menu == '1' :
+        quant_pastel = int(input("quantos pasteis deseja?\n> ")) 
+        pastel += quant_pastel
+
+    elif menu == '2' :
+        quant_açai = int(input("quatos litros de açai deseja?\n> "))
+        açai += quant_açai 
+
+    elif menu == '3' :
+        quant_xburguer = int(input("quantos xburguers deseja?\n> "))
+        xburger += quant_xburguer
